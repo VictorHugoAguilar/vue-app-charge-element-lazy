41372d5f233366be9e2b5418e9cc7d26500c42f6
Victor Hugo Aguilar <victorhugoaguilar@aol.com>
2022-11-16 10:31:49 +0100

    Add files via upload

---
+++ src/use/make-request.js
@@ -0,0 +1,20 @@
+import { ref } from 'vue'
+export default function () {
+  const loading = ref(false)
+  const error = ref(null)
+  async function startRequest (url) {
+    try {
+      const request = await fetch(url)
+      return await request.json()
+    } catch (error) {
+      error.value = error.toString()
+    } finally {
+      loading.value = false
+    }
+  }
+  return {
+    loading,
+    error,
+    startRequest
+  }
+}
